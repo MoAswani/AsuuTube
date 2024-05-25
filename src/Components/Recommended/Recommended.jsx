4d0d43aae4708a689b3396a7f1cb712113bb9a4e
Aswani <moaswani7@gmail.com>
2024-05-25 14:02:53 +0300

    Added API for recommended Video. The Side videos can now play with their respective data. Now, lets make the thing responsive using css media queries

--- src/Components/Recommended/Recommended.jsx
+++ src/Components/Recommended/Recommended.jsx
@@ -8,13 +8,14 @@ import thumbnail5 from "../../assets/thumbnail5.png";
 import thumbnail6 from "../../assets/thumbnail6.png";
 import thumbnail7 from "../../assets/thumbnail7.png";
 import thumbnail8 from "../../assets/thumbnail8.png";
-import { API_KEY } from "../../data";
+import { API_KEY, value_converter } from "../../data";
+import { Link } from "react-router-dom";
 
 const Recommended = ({ categoryId }) => {
   const [recommendedVideo, setRecommendedVideo] = useState([]);
 
   const fetchData = async () => {
-    const recommendedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=ke&maxResults=100&videoCategoryId=${categoryId}&key=${API_KEY}`;
+    const recommendedVideo_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=68&regionCode=ke&videoCategoryId=${categoryId}&key=${API_KEY}`;
     await fetch(recommendedVideo_url)
       .then((res) => res.json())
       .then((data) => setRecommendedVideo(data.items));
@@ -26,16 +27,20 @@ const Recommended = ({ categoryId }) => {
 
   return (
     <div className="recommended">
-      {recommendedVideo.map((index, item) => {
+      {recommendedVideo.map((item, index) => {
         return (
-          <div key={index} className="side-video-list">
-            <img src={thumbnail1} alt="" />
+          <Link
+            to={`/video/${item.snippet.categoryId}/${item.id}`}
+            key={index}
+            className="side-video-list"
+          >
+            <img src={item.snippet.thumbnails.medium.url} alt="" />
             <div className="vid-info">
-              <h4>Video Title</h4>
-              <p>ChannelName</p>
-              <p>250 Views</p>
+              <h4>{item.snippet.title}</h4>
+              <p>{item.snippet.channelTitle}</p>
+              <p>{value_converter(item.statistics.viewCount)} Views</p>
             </div>
-          </div>
+          </Link>
         );
       })}
     </div>
